7083954c21fe3c3cb5563bc086b5a512e48e1b66
alex-oh <oh.alexj@gmail.com>
2023-09-22 07:16:27 -0400

    initial commit map view component

---
+++ src/components/navigation/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./nav.css"; // Import the CSS
+
+function Nav() {
+
+    return (
+        <div>
+            <h1>navbar</h1>
+        </div>
+    );
+}
+
+export default Nav;
