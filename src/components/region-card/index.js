b709ab0363f059f83c71a522195233afa0187ed5
alex-oh <oh.alexj@gmail.com>
2023-09-22 07:16:55 -0400

    store details component init

---
+++ src/components/region-card/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import StoreDetails from "../store-details";
+
+function Region() {
+
+    return (
+        <div>
+            <h1>Region Card</h1>
+            <StoreDetails />
+            <StoreDetails />
+        </div>
+    );
+}
+
+export default Region;
