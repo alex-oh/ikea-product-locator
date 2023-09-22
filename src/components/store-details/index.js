3c5e6ff7b1fcefad313955a69c7fe66654312aa7
alex-oh <oh.alexj@gmail.com>
2023-09-22 07:17:21 -0400

    component init

---
+++ src/components/store-details/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function StoreDetails() {
+
+    return (
+        <div>
+            <h1>Store Details</h1>
+        </div>
+    );
+}
+
+export default StoreDetails;
---
+++ src/components/stores-info/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Region from "../region-card";
+
+function StoresInfo() {
+
+    return (
+        <div>
+            <h1>Stores Info Section</h1>
+            <Region />
+            <Region />
+        </div>
+    );
+}
+
+export default StoresInfo;
