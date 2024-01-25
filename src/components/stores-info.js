ad69a7f7a7d7b417123f1b8bff7f8439069bcc93
alex-oh <oh.alexj@gmail.com>
2024-01-25 00:02:19 -0500

    Restructure API calls in components - ikea API

--- src/components/stores-info.js
+++ src/components/stores-info.js
@@ -4,59 +4,12 @@ import "./stores-info.css";
 
 import StoreDetails from "./store-details";
 import MapView from "./map-view";
-import countries from "../data/countries.json";
 
 import { setDefaults } from "react-geocode";
 
-// import ikea availability checker library
-const checker = require("ikea-availability-checker");
-
-// search product by ID in all regions around the world, returns list of all stores and corresponding stock
-const getItem = async (countryCode, productId) => {
-    // make a list of all the stores in the region by country code
-    const stores = checker.stores.findByCountryCode(countryCode);
-
-    // get availabilities of product in each store in the region
-    const storesAvailable = JSON.stringify(
-        await checker.availabilities(stores, [productId])
-    );
-
-    // return json object of all stores with item in stock
-    return JSON.parse(storesAvailable);
-};
-
-const getCountryName = (countryCode) => {
-    // parses country name in countries.json from country code
-    for (let i = 0; i < countries.length; i++) {
-        let c = countries[i];
-        if (c.countryCode == countryCode) {
-            return c.name;
-        }
-    }
-    return countryCode;
-};
-
-function StoresInfo({ countryCode, itemId, onItemFound }) {
-    const [stores, setStores] = useState([]);
+function StoresInfo({ countryName, stores, onServiceCallback }) {
     const [service, setService] = useState(null);
     let className = "storesInfo";
-
-    const loadStores = async () => {
-        try {
-            const storesLoaded = await getItem(countryCode, itemId); // todo change literal to itemId
-            // convert storesLoaded to list
-            const storesList = Object.values(storesLoaded);
-            setStores(storesList);
-            if (storesList.length != 0) {
-                onItemFound(true);
-            } else {
-                onItemFound(false);
-            }
-        } catch (error) {
-            onItemFound(false);
-            console.log("item dne"); // todo: show feedback that item was searched unsuccessfully
-        }
-    };
 
     useEffect(() => {
         // set defaults for react geocode
@@ -67,14 +20,6 @@ function StoresInfo({ countryCode, itemId, onItemFound }) {
         });
     }, []);
 
-    useEffect(() => {
-        loadStores();
-    }, [countryCode, itemId]);
-
-    // useEffect(() => {
-    //     console.log("places service has been updated: ", service);
-    // }, [service]);
-
     // access each store in stores, and then pass to storedetails
     // TODO: fix child unique key render error with MapView component
     return (
@@ -82,24 +27,19 @@ function StoresInfo({ countryCode, itemId, onItemFound }) {
             <MapView storesList={stores} passPlacesService={setService} />
 
             <div className="storeDetailList">
-                <h1>{getCountryName(countryCode)}</h1>
-                {stores.length != 0 ? (
-                    stores
-                        .filter((s) => s.stock != 0)
-                        .map((store, i) =>
-                            service ? (
-                                <StoreDetails
-                                    key={store.createdAt}
-                                    storeInfo={store}
-                                    service={service}
-                                />
-                            ) : (
-                                <></>
-                            )
-                        )
-                ) : (
-                    <></>
-                )}
+                <h1>{countryName}</h1>
+                {stores.length != 0
+                    ? stores
+                          .map((store, i) =>
+                              service ? (
+                                  <StoreDetails
+                                      key={store.createdAt}
+                                      storeInfo={store}
+                                      service={service}
+                                  />
+                              ) : null
+                          )
+                    : null}
             </div>
         </div>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Nav from "../components/navigation";
 import StoresInfo from "../components/stores-info";
 import Search from "../components/search";
 
+import countries from "../data/countries.json";
+
+// import ikea availability checker library
+const checker = require("ikea-availability-checker");
+
+// search product by ID in all regions around the world, returns list of all stores and corresponding stock
+const getItem = async (countryCode, productId) => {
+    // make a list of all the stores in the region by country code
+    const stores = checker.stores.findByCountryCode(countryCode);
+
+    // get availabilities of product in each store in the region
+    const storesAvailable = JSON.stringify(
+        await checker.availabilities(stores, [productId])
+    );
+
+    // return json object of all stores with item in stock
+    return JSON.parse(storesAvailable);
+};
+
+const getCountryName = (countryCode) => {
+    // parses country name in countries.json from country code
+    for (let i = 0; i < countries.length; i++) {
+        let c = countries[i];
+        if (c.countryCode == countryCode) {
+            return c.name;
+        }
+    }
+    return countryCode;
+};
+
 function Home() {
-    const [country, setCountry] = useState("at");
+    const [countryCode, setCountryCode] = useState("at");
     const [itemId, setItemId] = useState("00402813");
     const [itemFound, setItemFound] = useState(false);
+    const [stores, setStores] = useState([]);
+
+    const loadStores = async () => {
+        try {
+            const storesLoaded = await getItem(countryCode, itemId); // todo change literal to itemId
+            // convert storesLoaded to list
+            const storesList = Object.values(storesLoaded);
+            setStores(storesList);
+            if (storesList.length != 0) {
+                setItemFound(true);
+            } else {
+                setItemFound(false);
+            }
+        } catch (error) {
+            setItemFound(false);
+            console.log("item dne");
+        }
+    };
+
+    useEffect(() => {
+        loadStores();
+    }, [countryCode, itemId]);
 
     // need to pull in data from ikea api here also. default to djungelskog
     return (
         <div>
             <Nav />
             <Search
-                onSelection={setCountry}
+                onSelection={setCountryCode}
                 onItemUpdate={setItemId}
-                defaultCountry={country}
+                defaultCountry={countryCode}
                 defaultItemId={itemId}
                 itemFoundStatus={itemFound} // gives us feedback on whether item was found in storesInfo component
             />
-            <StoresInfo countryCode={country} itemId={itemId} onItemFound={setItemFound} />
+            <StoresInfo countryName={getCountryName(countryCode)} stores={stores} />
         </div>
     );
 }
