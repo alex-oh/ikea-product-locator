0d68e3fc9b5bbb977b372b21b918aa7263463df4
alex-oh <oh.alexj@gmail.com>
2024-01-26 13:10:26 -0500

    Add error message check

--- src/pages/home.js
+++ src/pages/home.js
@@ -53,6 +53,7 @@ function Home() {
                 setItemFound(false);
             }
         } catch (error) {
+            console.log(error);
             setItemFound(false);
         }
     };
